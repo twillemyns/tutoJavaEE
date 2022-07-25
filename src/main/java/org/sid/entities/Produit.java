6b18646920993b8047e32ff7c24a4a7aa83f7409
Théo Willemyns <willemynstheo@gmail.com>
2022-07-25 16:28:48 +0200

    2e vidéo

--- src/main/java/org/sid/entities/Produit.java
+++ src/main/java/org/sid/entities/Produit.java
@@ -5,12 +5,20 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Entity
 public class Produit implements Serializable {
 	@Id 
 	@GeneratedValue
 	private Long id;
+	@NotNull
+	@Size(min=4,max=15)
 	private String designation;
+	@DecimalMin("100")
 	private double prix;
 	private int quantite;
 	public Produit() {
--- src/main/java/org/sid/web/ProduitController.java
+++ src/main/java/org/sid/web/ProduitController.java
@@ -2,6 +2,8 @@ package org.sid.web;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.sid.dao.ProduitRepository;
 import org.sid.entities.Produit;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -45,7 +48,10 @@ public class ProduitController {
 		return "FormProduit";
 	}
 	@RequestMapping(value="/save", method=RequestMethod.POST)
-	public String save(Model model, Produit produit) {
+	public String save(Model model, @Valid Produit produit, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "FormProduit";
+		}
 		produitRepository.save(produit);
 		return "Confirmation";
 	}
